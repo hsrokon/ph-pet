da91aa800fc254dfdfb413db4d74e96878b44309
hsrokon <hsrokonofficial@gmail.com>
2025-03-21 00:57:28 +0600

    Adoption button -not available rn- message added

--- scripts/home.js
+++ scripts/home.js
@@ -122,13 +122,14 @@ function renderPets(pets) {
             </div>
             <hr class="w-full text-slate-400">
             
-            <div class="flex items-center justify-evenly">
+            <div class="flex items-center justify-between">
                 <button class="border border-[#0E7A81] rounded-lg px-2 py-1.5 cursor-pointer" onclick="loadFavorites('${pet.petId}')">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>                                  
                 </button>
-                <button class="bg-[#0E7A81] w-auto border border-[#0E7A81] rounded-lg px-3 py-1.5 hover:bg-[#4d8c90] text-white cursor-pointer mx-auto">Adopt</button>
+                
+                <button class="bg-[#0E7A81] w-auto border border-[#0E7A81] rounded-lg px-3 py-1.5 hover:bg-[#4d8c90] text-white cursor-pointer" onclick="my_modal_2.showModal()">Adopt</button>
                 
                 <label for="my_modal_7" class="bg-[#0E7A81] w-auto border border-[#0E7A81] rounded-lg px-3 py-1.5 hover:bg-[#4d8c90] text-white cursor-pointer" onclick="petDetailsLoad(${pet.petId})">Details</label>
 
@@ -189,7 +190,6 @@ function renderFavorites() {
         `
         favDiv.appendChild(iFav)
     })
-
 }
 
 async function petDetailsLoad(petId) {
